20d190fc09a256bfa5076c08d0640dc218a4b211
srupani1987 <42297050+srupani1987@users.noreply.github.com>
2022-08-27 22:41:51 +0530

    test commit

--- src/arrays/PermutationOfEachOther.java
+++ src/arrays/PermutationOfEachOther.java
@@ -1,13 +1,12 @@
 package arrays;
 
 import java.util.Arrays;
-import java.util.IntSummaryStatistics;
 import java.util.OptionalInt;
 
 /**
  * You are given two integer arrays.
  * write a program to check if they are permutation of each other
- *
+ * <p>
  * if two arrays are said permutation, if both the arrays are having the same elements,
  * no matter where the element is (index)
  * Example:
@@ -20,7 +19,6 @@ public class PermutationOfEachOther {
                 new int[]{1, 3, 2, 4, 6, 5});
         System.out.println(isPermutation);
     }
-
     //Java8
     private static boolean isPermutation(int[] inArray1, int[] inArray2) {
         if (inArray1.length != inArray2.length) {
@@ -32,15 +30,12 @@ public class PermutationOfEachOther {
             OptionalInt product1 = Arrays.stream(inArray1).reduce((i1, i2) -> i1 * i2);
             OptionalInt product2 = Arrays.stream(inArray2).reduce((i1, i2) -> i1 * i2);
 
-            if (sum1 == sum2 && product1.getAsInt() == product2.getAsInt()) {
-                return true;
-            }
+            return (sum1 == sum2) && (product1.getAsInt() == product2.getAsInt());
         }
-        return false;
     }
 
     //Java 7
-    private static boolean isPermutation1(int[] inArray1, int[] inArray2) {
+    public static boolean isPermutation1(int[] inArray1, int[] inArray2) {
         if (inArray1.length != inArray2.length) {
             return false;
         }
@@ -49,7 +44,7 @@ public class PermutationOfEachOther {
         int mul1 = 1;
         int mul2 = 1;
 
-        for (int i=0; i< inArray1.length; i++) {
+        for (int i = 0; i < inArray1.length; i++) {
             sum1 += inArray1[i];
             sum2 += inArray2[i];
             mul1 *= inArray1[i];
